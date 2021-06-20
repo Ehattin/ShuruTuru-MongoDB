c05eedfa8d715a8fa95f1b708d5ee853dfe21c31
ayeletGi <ayelet47654@gmail.com>
2021-06-20 15:03:01 +0300

    crud tours

--- BackEnd/routes/tours.js
+++ BackEnd/routes/tours.js
@@ -11,28 +11,21 @@ module.exports = {
 
     /**  
      * Returns all availbale tours,
-     * sorted in ascending order by the tour's name.
      */
     getTours: function (req, res) {
-        // readFile(data => {
-        //         res.send(sortByIdAsc(data));
-        //     } );
+        Tour.find().then(tours =>
+            res.send(tours)
+        ).catch(e => res.status(500).send())
     },
     
     /** 
      * Returns a given tour details, including all the tours sites.
      */
     getTour: function (req, res) {
-        // const tourId = req.params["tour_id"];
-        // readFile(data => {
-        //     let index = findIndex(data,tourId);
-        //     if (index>=0){
-        //         res.send(data.data[index]);
-        //     }else{
-        //         res.status(400).send("Invalid tour id, tour doesn't exist.");
-        //     }
-        // },
-        // true);
+        const tourName = req.params["tour_id"];
+        Tour.findOne({ 'name':  tourName}).then(tour =>
+            res.send(tour)
+        ).catch(e => res.status(500).send())
     },
   
     /* ***************CREATE*************** */
@@ -40,13 +33,13 @@ module.exports = {
     /** 
      * This method gets:
      * Tour details- tour id, starting date, duration, price.
-     * Guide details- name, email, cellular. 
+     * GuideId - the guide reference in the guide collection. 
      * All fields are required to create a tour.
      * After adding the tour, the status is returns.
      */
     createTour: function (req, res) {
         const tour = new Tour(req.body)
-        tour.save().then(tour => {
+        tour.save().then(tou => {
             res.status(201).send(tour)
         }).catch(e => {
             res.status(400).send(e)
@@ -59,44 +52,25 @@ module.exports = {
      * Or/and a guide details- name, email, cellular. 
      */
      updateTour: function (req, res) {
-        // const tourId = req.params["tour_id"];
-    
-        // readFile( data => {
-        //     //check if tour exists
-        //     let index = findIndex(data,tourId);
-        //     if (index<0){
-        //         res.status(400).send("Tour doesn't exists.");
-        //         return;
-        //     }
-        
-          
-        //     //cannot change tour id
-        //     if(req.body.id){
-        //         res.status(400).send("Cannot change tour id.");
-        //         return;
-        //     }
-        //     //updating given fields
-        //     if(req.body.start_date)
-        //         data.data[index].start_date = req.body.start_date; 
-        //     if(req.body.duration)
-        //         data.data[index].duration = req.body.duration;
-        //     if(req.body.price)
-        //         data.data[index].price = req.body.price;
-        //     if(req.body.guide){
-        //         if(req.body.guide.name)
-        //             data.data[index].guide.name = req.body.guide.name;
-        //         if(req.body.guide.email)
-        //             data.data[index].guide.email = req.body.guide.email;
-        //         if(req.body.guide.cellular)
-        //             data.data[index].guide.cellular = req.body.guide.cellular;
-        //     }
-    
-        //     writeFile(JSON.stringify(data, null, 2), () => {
-        //         res.status(200).send("Tour was updated.");
-        //         return;
-        //     });
-        // }, true);
 
+        const tourName = req.params["tour_id"];
+        const updates = Object.keys(req.body)
+        const allowedUpdates = ['name', 'start_date', 'duration', 'price', 'guide']
+        const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+    
+        if (!isValidOperation) {
+            return res.status(400).send({ error: 'Invalid updates!' })
+        }
+    
+       Tour.updateOne( { name: tourName }, req.body, { new: true, runValidators: true }).then(user => {
+            if (!user) {
+                return res.status(404).send()
+            }
+            else {
+                console.log(user)
+                res.send(user)
+            }
+        }).catch(e => res.status(400).send(e))
     },
 
     /**
@@ -188,26 +162,17 @@ module.exports = {
      * Deletes a given tour.
      */
     deleteTour: function (req, res) {
-
-        // const tourId = req.params["tour_id"];
-
-        // readFile( data => {
-
-        //     //check if tour exists
-        //     let index = findIndex(data,tourId);
-        //     if (index<0){
-        //         res.status(400).send("Tour doesn't exists.");
-        //         return;
-        //     }
-
-        //     //deleting the tour
-        //     let filteredTours = data.data.filter(s => s.id != tourId);
-        //     data.data = filteredTours;
-
-        //     writeFile(JSON.stringify(data, null, 2), () => {
-        //         res.status(200).send("Tour was deleted.");
-        //         return;
-        //     });
-        // }, true);
+     
+        const tourName = req.params["tour_id"];
+    
+       Tour.deleteOne( { name: tourName }).then(user => {
+            if (!user) {
+                return res.status(404).send()
+            }
+            else {
+                console.log(user)
+                res.send(user)
+            }
+        }).catch(e => res.status(400).send(e))
     }
 };
